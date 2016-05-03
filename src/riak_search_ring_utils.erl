49ccaf5882757fad2613fd60a40e58e1b416bd26
Humberto Rodríguez Avila <rhumbertgz@gmail.com>
2016-05-03 23:58:06 +0200

    update for supporting new time API OTP 18

--- src/riak_search_ring_utils.erl
+++ src/riak_search_ring_utils.erl
@@ -44,7 +44,7 @@ get_covering_preflist(NVal) ->
 %% coverage. If node 3 AND 4 are down, then we need node 5 and node
 %% 2. It then picks out the nodes from the structure and returns the
 %% final unique preflist.
-%% 
+%%
 %% To create the structure, we first take the original set of X nodes,
 %% figure out how many iterations we need via ceiling(
 get_range_preflist(NVal, VNodes, UpNodes) ->
@@ -52,7 +52,7 @@ get_range_preflist(NVal, VNodes, UpNodes) ->
     UpNodesSet = ordsets:from_list(UpNodes),
 
     %% Randomly rotate the vnodes...
-    _ = random:seed(now()),
+    _ = random:seed(otp_utils:get_current_time()),
     RotationFactor = random:uniform(NVal),
     {Pre, Post} = lists:split(RotationFactor, VNodes),
     VNodes1 = Post ++ Pre,
@@ -61,28 +61,28 @@ get_range_preflist(NVal, VNodes, UpNodes) ->
     %% Create the preflist structure and then choose the preflist based on up nodes.
     Structure = create_preflist_structure(Iterations, NVal, VNodes1 ++ VNodes1),
     lists:usort(choose_preflist(Structure, UpNodesSet)).
-    
-create_preflist_structure(0, _NVal, _VNodes) -> 
+
+create_preflist_structure(0, _NVal, _VNodes) ->
     [];
-create_preflist_structure(Iterations, NVal, VNodes) -> 
+create_preflist_structure(Iterations, NVal, VNodes) ->
     {Backup, VNodes1} = lists:split(NVal, VNodes),
     {Primary, _} = lists:split(NVal, VNodes1),
     Group = [{hd(Primary), []}] ++ create_preflist_structure_1(tl(Primary), tl(Backup)),
     [Group|create_preflist_structure(Iterations - 1, NVal, VNodes1)].
-create_preflist_structure_1([], []) -> 
+create_preflist_structure_1([], []) ->
     [];
 create_preflist_structure_1([H|T], Backups) ->
     [{H, Backups}|create_preflist_structure_1(T, tl(Backups))].
-    
+
 %% Given a preflist structure, return the preflist.
 choose_preflist([Group|Rest], UpNodesSet) ->
     choose_preflist_1(Group, UpNodesSet) ++ choose_preflist(Rest, UpNodesSet);
-choose_preflist([], _) -> 
+choose_preflist([], _) ->
     [].
 choose_preflist_1([{Primary, Backups}|Rest], UpNodesSet) ->
     {_, PrimaryNode} = Primary,
     AvailableBackups = filter_upnodes(Backups, UpNodesSet),
-    case ordsets:is_element(PrimaryNode, UpNodesSet) of 
+    case ordsets:is_element(PrimaryNode, UpNodesSet) of
         true when AvailableBackups == [] ->
             [Primary];
         true when AvailableBackups /= [] ->
@@ -90,13 +90,13 @@ choose_preflist_1([{Primary, Backups}|Rest], UpNodesSet) ->
         false ->
             choose_preflist_1(Rest, UpNodesSet)
     end;
-choose_preflist_1([], _) -> 
+choose_preflist_1([], _) ->
     [].
 
 %% Given a list of VNodes, filter out any that are offline.
 filter_upnodes([{Index,Node}|VNodes], UpNodesSet) ->
     case ordsets:is_element(Node, UpNodesSet) of
-        true -> 
+        true ->
             [{Index, Node}|filter_upnodes(VNodes, UpNodesSet)];
         false ->
             filter_upnodes(VNodes, UpNodesSet)
@@ -114,7 +114,7 @@ ceiling(Numerator, Denominator) ->
 % be used to calculate the partition on the ring. It is used in places
 % where we need to make repeated calls to get the actual partition
 % (not the DocIdx) of an Index/Field/Term combination. NOTE: This, or something like it,
-% should probably get moved to Riak Core in the future. 
+% should probably get moved to Riak Core in the future.
 -define(RINGTOP, trunc(math:pow(2,160)-1)).  % SHA-1 space
 
 zip_with_partition_and_index(Postings) ->
